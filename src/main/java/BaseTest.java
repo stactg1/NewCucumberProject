09150916fb0dc793fef007365aa0e4d04b3853d8
water <water@waters-MacBook-Pro.local>
2018-11-26 19:10:48 -0500

    adding a projecrt

---
+++ src/main/java/BaseTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+	WebDriver dr;
+	public void openBrowser(string browser)
+	{
+		if(browser.equals("chrome"))
+			System.setProperty("webdriver.chrome.driver", "/Users/water/Downloads/chromedriver");
+		dr = new ChromeDriver();
+	
+	}
+	
+	
+	public goToURL() {
+		dr.get("https://www.eshopper24.com/");
+		return new goToURL(dr);
+	}
+
+}
---
+++ src/main/java/Stepdefination.java
@@ -0,0 +1,41 @@
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class Stepdefination {
+	@Given("^OpenBrowser$")
+	public void openbrowser(DataTable arg1) throws Throwable {
+	    
+	   
+	}
+
+	@Given("^navigate to URL$")
+	public void navigate_to_URL() throws Throwable {
+		
+	}
+	   
+
+	@When("^i click on menu$")
+	public void i_click_on_menu() throws Throwable {
+	  
+	}
+
+	@When("^i click silk saree$")
+	public void i_click_silk_saree() throws Throwable {
+	  
+	}
+
+	@When("^i click first product$")
+	public void i_click_first_product() throws Throwable {
+	    
+	}
+
+	@When("^select color$")
+	public void select_color() throws Throwable {
+	    
+	}
+
+
+
+}
